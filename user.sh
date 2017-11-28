b19e199583558c24189db39a6f93a98c689bc066
Andreas Leibrock <aleibrock@vwd.com>
2017-11-28 08:33:13 +0100

    make sure we cut the string from passwd to get the username typo corrected

--- user.sh
+++ user.sh
@@ -5,12 +5,12 @@ if [ $# -lt 3 ]
 		echo "Zu wenige Parameter angegeben. Es müssen Nutzername, Kommentar und Passwort angegeben werden" #Fehlermeldung wenn nur ein Parameter angegeben ist
 		exit 1
 	else
-	if [ $(cat /etc/passwd | grep $1 | wc -w) -gt 0 ]										#Prüfen ob Nutzer schon besteht
+	if [ $(cat /etc/passwd | cut -f1 -d: | grep $1 | wc -w) -gt 0 ]										#Prüfen ob Nutzer schon besteht
 		then
 			echo "Nutzer besteht bereits"													#Fehlermeldung ausgeben das der Nutzer bereits existiert
 			exit 2
 		else
-			echo "$1:x:501:501:$2:/home/$1:/bin/bash" >> /etc/passwd						#Nutzer ini passwd Datei anlegen -> Name:Passwort:User-ID:Group-ID:Kommentar:Verzeichnis:Shell  
+			echo "$1:x:501:501:$2:/home/$1:/bin/bash" >> /etc/passwd						#Nutzer in passwd Datei anlegen -> Name:Passwort:User-ID:Group-ID:Kommentar:Verzeichnis:Shell  
 			echo "$1:x:501:" >> /etc/group													#Gruppe für Nutzer anlegen
 			mkdir /home/$1																	#Homeverzeichnis anlegen 
 			chown $1:$1 /home/$1															#Nutzer die Besitzrechte auf das Verzeichnis übertragen

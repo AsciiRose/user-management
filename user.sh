06a2f7ada12ac05f72964fdd2c469d06883befc1
Alexandra Matthes <amatthes@vwd.com>
2018-01-12 08:14:09 +0100

    Edit name

--- user.sh
+++ user.sh
@@ -5,84 +5,97 @@
 source env.sh
 source function.sh
 
-if [ $# -lt 4 ]
+if [ $# -lt 1 ]
 then
     #Fehlermeldung wenn zu wenige Parameter angegeben sind
-	echo "Zu wenige Parameter angegeben. Es müssen Nutzername, Gruppenname, Kommentar und Passwort angegeben werden"
+	echo "Keine Parameter übergeben. Übergeben Sie einen oder mehrere Benutzernamen ein."
 	#Abbrechen
 	return 1
 else
-	#Setzt Pfade über eine andere Datei
-	setEnv
-
-	#Variablen für die Übergabewerte
-	inputUserName=$1
-	inputGroupName=$2
-	inputComment=$3
-	inputPassword=$4
-
-    #Prüfen ob Nutzer schon besteht
-	searchUser $inputUserName
+	for [ username in $* ]
+	do
+		if [ -z $groupname ]
+		then
+			groupname=$username
+		fi
 	
-    #Brüft ob es einen Fehler gab
-	if [ ! $? -eq 0 ]
-	then
-        #Abbrechen
-		exit 2
-	else
-        #Sucht eine neue freie User ID
-		getNewID $userFileName
-		
-        #Brüft ob es einen Fehler gab
-		if [ ! $? -eq 0 ]
-	    then
-			echo "getNewID Error"
-			
-            #Abbrechen
-			exit 3
-		else
-			#Speichert ID in einer Variable
-			userID=$ID
-			
-            #Sucht eine neue freie Group ID
-			getNewID $groupFileName
+		#Setzt Pfade über eine andere Datei
+		setEnv $username
 
-            #Brüft ob es einen Fehler gab
+		#Prüfen ob Nutzer schon besteht
+		searchUser $username
+		
+		#Brüft ob es einen Fehler gab
+		if [ ! $? -eq 0 ]
+		then
+			#Abbrechen
+			exit 2
+		else
+			#Sucht eine neue freie User ID
+			getNewID $userFileName
+			
+			#Brüft ob es einen Fehler gab
 			if [ ! $? -eq 0 ]
 			then
 				echo "getNewID Error"
-			
-                #Abbrechen
-				exit 4
+				
+				#Abbrechen
+				exit 3
 			else
 				#Speichert ID in einer Variable
-				groupID=$ID
+				userID=$ID
 				
-                #Prüft ob Gruppe bereits existiert
-                #Legt ggf. eine neue Gruppe an
-                checkAndAddGroup $inputGroupName $groupID
-				
-				if [ ! -z $groupNumber ] 
+				#Sucht eine neue freie Group ID
+				getNewID $groupFileName
+
+				#Brüft ob es einen Fehler gab
+				if [ ! $? -eq 0 ]
 				then
-					groupID=$groupNumber
+					echo "getNewID Error"
+				
+					#Abbrechen
+					exit 4
+				else
+					#Speichert ID in einer Variable
+					groupID=$ID
+					
+					#Prüft ob Gruppe bereits existiert
+					#Legt ggf. eine neue Gruppe an
+					checkAndAddGroup $groupName $groupID
+					
+					if [ ! -z $groupNumber ] 
+					then
+						groupID=$groupNumber
+					fi
+					
+					if [ -z $loginshell ]
+					then
+						loginshell="/bin/bash"
+					fi
+
+					#Nutzer in passwd Datei anlegen -> Name:Passwort:User-ID:Group-ID:Kommentar:Verzeichnis:Shell
+					echo "$username:x:$userID:$groupID:$3:$homePath$username:$loginshell" >> $userPath
+
+					#Homeverzeichnis anlegen
+					mkdir -p $homePath$username
+
+					#Nutzer die Besitzrechte auf das Verzeichnis übertragen -> chown Benutzer:Gruppe Datei
+					chown $username:$groupName $homePath$username
+					#Nutzer Schreibrechte in eigenem Homeverzeichnis geben
+					chmod 700 $homePath$username
+					
+					getRandomPassword
+
+					#Passwort für Nutzer anlegen
+					echo -e $password | ($userFileName $username)
+					
+					echo "Ihr Passwort zu dem Benutzer $username lautet zur Zeit: $password"
+					
+					echo "$username, $password" >> user.csv
 				fi
-
-                #Nutzer in passwd Datei anlegen -> Name:Passwort:User-ID:Group-ID:Kommentar:Verzeichnis:Shell
-				echo "$inputUserName:x:$userID:$groupID:$3:$homePath$inputUserName:/bin/bash" >> $userPath
-
-                #Homeverzeichnis anlegen
-				mkdir -p $homePath$inputUserName
-
-                #Nutzer die Besitzrechte auf das Verzeichnis übertragen -> chown Benutzer:Gruppe Datei
-				chown $inputUserName:$inputGroupName $homePath$inputUserName
-				#Nutzer Schreibrechte in eigenem Homeverzeichnis geben
-				chmod 700 $homePath$inputUserName
-
-                #Passwort für Nutzer anlegen
-				echo -e "$4\n$4" | ($userFileName $inputUserName)
 			fi
 		fi
-	fi
+	done
 fi
 
 
